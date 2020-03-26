aa465a46540a5ebbb5948521e2ba153ebfa203fb
slischewski@munichre.com <slischewski@munichre.com>
2020-03-26 21:07:14 +0100

    fetaure(tree): inlc. plotted tree

--- learn.py
+++ learn.py
@@ -3,9 +3,11 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.model_selection import KFold, cross_val_score
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier, plot_tree
+import matplotlib.pyplot as plt
 
-titanic_data = pd.read_csv("../data/titanic.csv")
+
+titanic_data = pd.read_csv("./data/titanic.csv")
 labelEncoder = LabelEncoder()
 titanic_data['Sex'] = labelEncoder.fit_transform(titanic_data['Sex'])
 
@@ -19,6 +21,11 @@ y = X.pop('Survived')
 
 model = XGBClassifier()
 kfold = KFold(n_splits=10)
-results = cross_val_score(model, X, y, cv=kfold)
+scores = cross_val_score(model, X, y, cv=kfold, scoring='accuracy')
+print(scores)
 
-print(results)
+model.fit(X, y)
+p
+# plot single tree
+plot_tree(model)
+plt.show()
