6e4dc1fbc5421bac930a26346477d46ce7337583
slischewski@munichre.com <slischewski@munichre.com>
2020-03-26 20:30:48 +0100

    feature(init model): first version with titanic data

---
+++ learn.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.model_selection import KFold, cross_val_score
+from xgboost import XGBClassifier
+
+titanic_data = pd.read_csv("../data/titanic.csv")
+labelEncoder = LabelEncoder()
+titanic_data['Sex'] = labelEncoder.fit_transform(titanic_data['Sex'])
+
+# first try without name
+titanic_data.drop(['Name'], axis=1, inplace=True)
+
+# random split
+msk = np.random.rand(len(titanic_data)) < 0.8
+X = titanic_data[msk]
+y = X.pop('Survived')
+
+model = XGBClassifier()
+kfold = KFold(n_splits=10)
+results = cross_val_score(model, X, y, cv=kfold)
+
+print(results)
